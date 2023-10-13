837213bcd8a1cca184c457116ccd1398673461d6
28emilyma <28emilyma@gmail.com>
2023-10-13 00:15:56 -0400

    added addReservation and displayReservation - hw due 10/13

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,14 +1,39 @@
 public class ReservationSystem {
-
+    public Reservation[] reservations;
     public static void main(String[] args) {
         ReservationSystem jarvis = new ReservationSystem();
     }
 
     public ReservationSystem(){
-        Reservation gavin = new Reservation(4, 7.30, 2, 10);
-        gavin.print();
+        Reservation gavin = new Reservation(6, 10.00, 3, 200);
+
+        Reservation dog = new Reservation(4, 7.30, 2, 100);
+        //dog.print();
+        reservations = new Reservation[10];
+        reservations[0] = gavin;
+        reservations[1] = dog;
+
+        //Reservation cat = new Reservation(2,8.00,1,90);
+        //reservations[2] = cat;
+
+        reservations[2] = new Reservation(2,8.00,1,90);
+
+        for(int i = 3; i < 9; i++){
+            reservations[i] = new Reservation(2,7.00,2, 6);
+        }
+        addReservation(5,5.00,5,500);
+        displayReservations();
+    }
+    public void addReservation(int numPeople, double time, int priority, long timeMade){
+        Reservation newReservation = new Reservation(numPeople, time, priority, timeMade);
+        reservations[9]=newReservation;
+        //^how do i do this without specifying at index 9
     }
 
 
-
+    public void displayReservations(){
+        for(int i = 0; i < 10; i++){
+            reservations[i].print();
+        }
+    }
 }
