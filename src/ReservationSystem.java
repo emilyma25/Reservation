38d6e9ddaa471cea7fc5f7eb040d23095d42766d
28emilyma <28emilyma@gmail.com>
2023-10-15 21:24:34 -0400

    new addDisplay method

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -5,35 +5,43 @@ public class ReservationSystem {
     }
 
     public ReservationSystem(){
-        Reservation gavin = new Reservation(6, 10.00, 3, 200);
-
-        Reservation dog = new Reservation(4, 7.30, 2, 100);
+//        Reservation gavin = new Reservation(6, 10.00, 3, 200);
+//
+//        Reservation dog = new Reservation(4, 7.30, 2, 100);
         //dog.print();
         reservations = new Reservation[10];
-        reservations[0] = gavin;
-        reservations[1] = dog;
-
-        //Reservation cat = new Reservation(2,8.00,1,90);
-        //reservations[2] = cat;
-
-        reservations[2] = new Reservation(2,8.00,1,90);
-
-        for(int i = 3; i < 9; i++){
-            reservations[i] = new Reservation(2,7.00,2, 6);
-        }
-        addReservation(5,5.00,5,500);
+//        reservations[0] = gavin;
+//        reservations[1] = dog;
+//
+//        //Reservation cat = new Reservation(2,8.00,1,90);
+//        //reservations[2] = cat;
+//
+//        reservations[2] = new Reservation(2,8.00,1,90);
+//
+//        for(int i = 3; i < 9; i++){
+//            reservations[i] = new Reservation(2,7.00,2, 6);
+//        }
+        addReservation(5, 5.00, 5, 500);
         displayReservations();
     }
     public void addReservation(int numPeople, double time, int priority, long timeMade){
         Reservation newReservation = new Reservation(numPeople, time, priority, timeMade);
-        reservations[9]=newReservation;
-        //^how do i do this without specifying at index 9
+        for(int i = 0; i < reservations.length; i++){
+            if(reservations[i]==null){
+                reservations[i]=newReservation;
+                reservations[i].print();
+                break;
+            }
+        }
     }
 
-
     public void displayReservations(){
-        for(int i = 0; i < 10; i++){
-            reservations[i].print();
+        for(int i = 0; i < reservations.length; i++) {
+            if (reservations[i] != null) {
+                reservations[i].print();
+            } else{
+                System.out.println("null");
+            }
         }
     }
 }
